e1b7a58d7e0d701c491e3db49f52dded85ff142d
nimxyz <michaelnimah@gmail.com>
2022-10-13 23:18:50 +0000

    betty is going to have to accept this

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,16 +4,14 @@
 /**
  *main - Entry point
  *
- *Description: 'print and that piece of art is useful" - Dora Korpar, 2015-1
- * 0-19'
+ *Description: 'print and that piece of art is useful" - Dora Korpar, 2015-10-19'
  *
  *Return: Always 0 (Success)
  **/
 
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-
-		10-19");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (0);
 }
 
